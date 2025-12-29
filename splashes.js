3f82d3f03040915ac12ffba5f9fd9ba113e30a8f
Yangsy56302 <yangsy56302@163.com>
2025-12-29 11:10:38 +0800

    splash format

--- splashes.js
+++ splashes.js
@@ -1,157 +1,186 @@
 const splashes = [
     // technical
-    // "* insert random splashes here *",
-    "test message pls ingore",
+    // "* insert random splashes here *`,
+    `test message pls ingore`,
     
     // uncategorized
 
     // ADOFAI
 
-    "A Dance of Fire and Ice!",
-    "Playing it Straight!",
-    "North and South are Always Off Beat!",
-    "SSSSSS!",
-    "More SSSSSS!",
-    "These Tutorials Are Inadequate for What's Coming, Forgive Me!",
-    "Good Luck!",
-    "SSSSSS Returns!",
-    "Speed Trial!",
-    "Swirly SSSSSS!",
-    "Inside Squares, Outside Squares!",
-    "Spin 2 Win!",
-    "Introducing Hexagons!",
-    "The Beast With Many Eyes!",
-    "ZZZZZZ!",
-    "Behold, The Right Triangle!",
-    "North and South are Now On-Beat!",
-    "Crumbling Boxes!",
-    "For Added Flavor, Add Loops!",
-    "Introducing Midspin Tiles!",
-    "Trust Your Ears, not Your Eyes!",
-    "Miko Skip!",
-    "BBBB!",
-    "EMOMOMO!",
-    "U-turn Road!",
-    "One Forgotten Night!",
-    "It Go!",
-    "Elevators!",
-    "Elevator Dance!",
-    "Unlimited Flexibility!",
-    "NEO COSMOS!",
-    "Introducing The Hold!",
-    "Catch and Release!",
-    "NEW LIFE!",
-    "Catch and Release, Sixteenths!",
-    "No Holds Barred!",
-    "OK Maybe One Hint Here!",
-    "No Hints Here!",
-    "Disappearing Floor!!",
-    "Almost there!! Don't get distracted!!",
-    "The J!",
-    "To Three To Two!",
-    "My New Updated Maze (final) (1)",
+    `A Dance of Fire and Ice!`,
+    `Playing it Straight!`,
+    `North and South are Always Off Beat!`,
+    `SSSSSS!`,
+    `More SSSSSS!`,
+    `These Tutorials Are Inadequate for What's Coming, Forgive Me!`,
+    `Good Luck!`,
+    `SSSSSS Returns!`,
+    `Speed Trial!`,
+    `Swirly SSSSSS!`,
+    `Inside Squares, Outside Squares!`,
+    `Spin 2 Win!`,
+    `Introducing Hexagons!`,
+    `The Beast With Many Eyes!`,
+    `ZZZZZZ!`,
+    `Behold, The Right Triangle!`,
+    `North and South are Now On-Beat!`,
+    `Crumbling Boxes!`,
+    `For Added Flavor, Add Loops!`,
+    `Introducing Midspin Tiles!`,
+    `Trust Your Ears, not Your Eyes!`,
+    `Miko Skip!`,
+    `BBBB!`,
+    `EMOMOMO!`,
+    `U-turn Road!`,
+    `One Forgotten Night!`,
+    `It Go!`,
+    `Elevators!`,
+    `Elevator Dance!`,
+    `Unlimited Flexibility!`,
+    `NEO COSMOS!`,
+    `Introducing The Hold!`,
+    `Catch and Release!`,
+    `NEW LIFE!`,
+    `Catch and Release, Sixteenths!`,
+    `No Holds Barred!`,
+    `OK Maybe One Hint Here!`,
+    `No Hints Here!`,
+    `Disappearing Floor!!`,
+    `Almost there!! Don't get distracted!!`,
+    `The J!`,
+    `To Three To Two!`,
+    `My New Updated Maze (final) (1)`,
     
     // ADOFAI Cursed by Firestix
-    "A Dance of Fire and Ice but the levels are <a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/crab/index.html\">curse</a>d!",
-    "It's a little <a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/crab/index.html#2-X\">longer</a>!",
-    "<a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/crab/index.html#3-X\">!eef etal ruoy yap ot tegrof t'noD</a>",
-    "Keep having <a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/crab/index.html#4-X\">nightmares about Classic Pursuit</a>!",
-    "Something is wrong with <a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/crab/index.html#5-X\">the train</a>!",
-    "Are you bored with normal ADOFAI? Are you one of the chosen few who can ace PULSE first try? Well, level <a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/crab/index.html#6-X\">6-X?</a> is for you!",
-    "Please continue playing Firestix's <a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/crab/index.html#B-X\">level</a>s!",
-    "<a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/crab/index.html#B-X\">The final battle</a> has begun!",
-    "<a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/crab/index.html#7-X\">There</a>'s no fun allowed!",
-    "If you play the drums or are good at rhythm, <a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/crab/index.html#8-X\">8-X?</a> will make you want to die!",
-    "There's <a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/crab/index.html#11-X\">a twirl on every tile</a>!",
-    "You must <a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/crab/index.html#11-X\">fight The Midnight King</a> in order to free the crabs from it's tyranny!",
-    "haha <a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/crab/index.html#12-X\">funny song</a> go du dudulu dudulu dudulu dudulu dudulu dudulu dudulu",
-    "<a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/crab/index.html#PA-X\">Way too many midspins</a>!",
-    "<a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/crab/index.html#PA-X\">h</a>",
-    "<a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/crab/index.html#XO-X\">One Forgotten Meme</a>!",
-    "Rid <a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/crab/index.html#XO-X\">the world</a> of these curses once and for all!",
-    "<a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/crab/index.html#XT-X\">Stoptions</a>!",
-    "It's a <a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/crab/index.html#MN-X\">crossover</a>!",
-    "I don't think <a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/crab/index.html#ML-X\">this castle</a> is up to code at all...",
+    `A Dance of Fire and Ice but the levels are <a target="_blank" href="{root}/adofai/cursed/crab/index.html">curse</a>d!`,
+    `It's a little {2-X?}longer</a>!`,
+    `{3-X?}!eef etal ruoy yap ot tegrof t'noD</a>`,
+    `Keep having {4-X?}nightmares about Classic Pursuit</a>!`,
+    `Something is wrong with {5-X?}the train</a>!`,
+    `Are you bored with normal ADOFAI? Are you one of the chosen few who can ace PULSE first try? Well, level {6-X?}6-X?</a> is for you!`,
+    `Please continue playing Firestix's {B-X?}level</a>s!`,
+    `{B-X?}The final battle</a> has begun!`,
+    `{7-X?}There</a>'s no fun allowed!`,
+    `If you play the drums or are good at rhythm, {8-X?}8-X?</a> will make you want to die!`,
+    `There's {11-X?}a twirl on every tile</a>!`,
+    `You must {11-X?}fight The Midnight King</a> in order to free the crabs from it's tyranny!`,
+    `haha {12-X?}funny song</a> go du dudulu dudulu dudulu dudulu dudulu dudulu dudulu`,
+    `{PA-X?}Way too many midspins</a>!`,
+    `{PA-X?}h</a>`,
+    `{XO-X?}One Forgotten Meme</a>!`,
+    `Rid {XO-X?}the world</a> of these curses once and for all!`,
+    `{XT-X?}Stoptions</a>!`,
+    `It's a {MN-X?}crossover</a>!`,
+    `I don't think {ML-X?}this castle</a> is up to code at all...`,
 
     // ADOFAI Cursed by Yangsy56302
-    "OMG even the lagtrain would be smoother than <a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/string/e.html#3-X\">this</a>!",
-    "<a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/string/e.html#4-X\">Short</a>!",
-    "There is <a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/string/e.html#6-X\">a 1 beat PAUSE on every tile</a>!",
-    "<a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/string/e.html#B-X\">OOooppss......</a>",
-    "Spin 2 Win <a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/string/e.html#7-X\">is 2 slow</a>!",
-    "This cliff is <a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/string/e.html#8-X\">made of living wood</a>!",
-    "Did anyone actually go to <a target=\"_blank\" href=\"https://tauday.com/\">tauday.com</a>?",
-    "but <a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/string/e.html#11-X\">there is no heracles</a> lol",
-    "There are no shortcuts in real life, but <a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/string/e.html#XF-X\">in game</a> that's okay I guess!",
-    "<a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/string/e.html#XC-X\">Tutorial Exclusive</a>!",
-    "*<a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/string/e.html#PA-X\">high-pitched water drop sound</a>*",
-    "<a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/string/e.html#XR-X\">Triangular</a>, inconsistent, complicated, difficult and cute rabbits!",
-    "The 99th civilization at Proxima Centauri b was doomed because <a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/string/e.html#XN-X\">3 planets appeared in the sky on their planet at the same time</a>!",
-    "Subscribe to <a target=\"_blank\" href=\"https://www.youtube.com/@OokamiOkoku\">Ouka Miko</a>!",
-    "<a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/string/e.html#XS-X\">Delayed</a> by 1 tile!",
-    "Since we already had tutorial for curses, why don't we try to make <a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/string/e.html#XO-1\">curses for tutorial</a>?",
-    "<a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/string/e.html#6-X\">PAUSE</a>: <a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/string/e.html#XI-X\">Limited Edition</a>!",
-    "<a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/string/e.html#XI-X\">1 Go!</a>",
-    "All the straight tiles are <a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/string/e.html#XT-X\">missing</a>!",
-    "<a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/string/e.html#T4-X\">Midspin</a> 2 Win!",
-    // "Non-Explosive Memes!",
-    "<a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/string/a.html#Arbitrary_Character\">Arbitrary Character</a>!",
-    "Nobody said that a level must choose at most one between <a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/string/e.html#XS-X\">becoming a curse</a> and <a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/string/a.html#Actions\">being cursed</a>, so yeah!",
-    "<a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/string/a.html#Party_of_stiripS\">Party of !stiripS</a>",
-    "<a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/string/a.html#D_is_isinstance\">D is isinstance</a>!", 
-    "<a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/string/a.html#EMCLL\">EEEEVEN MOMOMORE CURSED LED LEVELS</a>!",
-    "<a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/string/a.html#14G10R\">1 4 Got 10 →</a>!",
-    "Hope you guys still feels like a piece of cake to <a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/string/a.html#Git0\">deal with fundamental math operations quickly</a>!", 
-    "<a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/string/a.html#Define_Interaction\">Define Interaction</a>!",
-    "<a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/string/a.html#Novel_Feature\">Novel Feature</a>!",
+    `OMG even the lagtrain would be smoother than {"3-X"}this</a>!`,
+    `{"4-X"}Short</a>!`,
+    `There is {"6-X"}a 1 beat PAUSE on every tile</a>!`,
+    `{"B-X"}OOooppss......</a>`,
+    `Spin 2 Win {"7-X"}is 2 slow</a>!`,
+    `This cliff is {"8-X"}made of living wood</a>!`,
+    `Did anyone actually go to <a target="_blank" href="https://tauday.com/">tauday.com</a>?`,
+    `but {"11-X"}there is no heracles</a> lol`,
+    `There are no shortcuts in real life, but {"XF-X"}in game</a> that's okay I guess!`,
+    `{"XC-X"}Tutorial Exclusive</a>!`,
+    `*{"PA-X"}high-pitched water drop sound</a>*`,
+    `{"XR-X"}Triangular</a>, inconsistent, complicated, difficult and cute rabbits!`,
+    `The 99th civilization at Proxima Centauri b was doomed because {"XN-X"}3 planets appeared in the sky on their planet at the same time</a>!`,
+    `Subscribe to <a target="_blank" href="https://www.youtube.com/@OokamiOkoku">Ouka Miko</a>!`,
+    `{"XS-X"}Delayed</a> by 1 tile!`,
+    `Since we already had tutorial for curses, why don't we try to make {"XO-1"}curses for tutorial</a>?`,
+    `{"6-X"}PAUSE</a>: {"XI-X"}Limited Edition</a>!`,
+    `{"XI-X"}1 Go!</a>`,
+    `All the straight tiles are {"XT-X"}missing</a>!`,
+    `{"T4-X"}Midspin</a> 2 Win!`,
+    // "Non-Explosive Memes!`,
+    `{Arbitrary_Character "L"}Arbitrary Character</a>!`,
+    `Nobody said that a level must choose at most one between {"XS-X"}becoming a curse</a> and {Actions "L"}being cursed</a>, so yeah!`,
+    `{Party_of_stiripS "L"}Party of !stiripS</a>`,
+    `{D_is_isinstance "L"}D is isinstance</a>!`, 
+    `{EMCLL "L"}EEEEVEN MOMOMORE CURSED LED LEVELS</a>!`,
+    `{14G10R "L"}1 4 Got 10 →</a>!`,
+    `Hope you guys still feels like a piece of cake to {Git0 "L"}deal with fundamental math operations quickly</a>!`, 
+    `{Define_Interaction "L"}Define Interaction</a>!`,
+    `{Novel_Feature "L"}Novel Feature</a>!`,
 
     // ADOFAI Cursed
-    "Exclamation mark!",
+    `Exclamation mark!`,
 
     // ADOFAI Cursed Notations
-    "Your current speed is 60 seconds per minute",
-    "You are now experiencing 0 extra tiles per beat",
-    "Current curse: None",
-    "Next curse: ...",
-    "Current level: <a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/string/a.html#D_is_isinstance\">\"11-X\"(\"XT-X\"(PA-X))</a>",
-    "Next level: XT-6",
-    "<a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/index.html#None\">None</a> = λx.x",
+    `Your current speed is 60 seconds per minute`,
+    `You are now experiencing 0 extra tiles per beat`,
+    `Current curse: None`,
+    `Next curse: ...`,
+    `Current level: {D_is_isinstance "L"}"11-X"("XT-X"(PA-X))</a>`,
+    `Next level: XT-6`,
+    `<a target="_blank" href="{root}/adofai/cursed/index.html#None">None</a> = λx.x`,
 
     // ADOFAI Cursed Mathematical Models
-    "<a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/index.html#Functional\">Function composition</a>!",
-    "<a target=\"_blank\" href=\"https://yangsy56302.github.io/adofai/cursed/index.html#^-1\">Inverse function</a>!",
-    "Matrix!",
-    "Lambda calculus!",
+    `<a target="_blank" href="{root}/adofai/cursed/index.html#Functional">Function composition</a>!`,
+    `<a target="_blank" href="{root}/adofai/cursed/index.html#^-1">Inverse function</a>!`,
+    `Matrix!`,
+    `Lambda calculus!`,
 
     // DDLC
-    "Doki Doki Literature Club!",
-    "Just Monika!",
-    "Monika After Story!",
-    "MAKE IT STOP!",
-    "print(os.listdir('./characters/'), sep='\\n', end='\\n\\n')",
+    `Doki Doki Literature Club!`,
+    `Just Monika!`,
+    `Monika After Story!`,
+    `MAKE IT STOP!`,
+    `print(os.listdir('./characters/'), sep='\\n', end='\\n\\n')`,
 
     // DDLC x BA
-    "Doki Doki Heart Milestone Event!",
-    "Rotating, Translating, Rewinding, and Scaling!",
-    "yuuka.obj.mass = 100 * si.common.kilo",
+    `Doki Doki Heart Milestone Event!`,
+    `Rotating, Translating, Rewinding, and Scaling!`,
+    `yuuka.obj.mass = 100 * si.common.kilo`,
     
     // BA
-    "Vol1.3 sucks!",
+    `Vol1.3 sucks!`,
 
     // BIY
-    "Baba Is You!",
-    "text_what what",
-    "text_make text_it text_stop woah",
+    `Baba Is You!`,
+    `text_what what`,
+    `text_make text_it text_stop woah`,
 
     // Minecraft
 
     // Website
-    "Press F5 to refresh the page (and of course this splash)!",
-    "!<a target=\"_blank\" href=\"https://yangsy56302.github.io/404.html\">404</a> not Not Found!"
+    `Press F5 to refresh the page (and of course this splash)!`,
+    `!<a target="_blank" href="{root}/404.html">404</a> not Not Found!`,
 ];
 
+function replaceSplash(str) {
+    splash.replaceAll(
+        /\{([\w]+-[\w]+)\?\}/, 
+        `<a target="_blank" href="{root}/adofai/cursed/crab/index.html#$1">`
+    );
+    splash.replaceAll(
+        /\{([\w]+-[\w]+)\?'\}/, 
+        `<a target="_blank" href="{root}/adofai/cursed/crab/shifted.html#$1">`
+    );
+    splash.replaceAll(
+        /\{([\w]+-[\w]+)\*\}/, 
+        `<a target="_blank" href="{root}/adofai/cursed/landmine/e.html#$1">`
+    );
+    splash.replaceAll(
+        /\{([^ ]+) L\*\}/, 
+        `<a target="_blank" href="{root}/adofai/cursed/landmine/a.html#$1">`
+    );
+    splash.replaceAll(
+        /\{"([\w]+-[\w]+)"\}/, 
+        `<a target="_blank" href="{root}/adofai/cursed/string/e.html#$1">`
+    );
+    splash.replaceAll(
+        /\{([^ ]+) "L"\}/, 
+        `{$1 "L"}`
+    );
+    splash.replaceAll(`{root}`, `https://yangsy56302.github.io`);
+    return str;
+}
 function randomSplash() {
-    document.getElementById("splash").innerHTML = splashes[Math.floor(Math.random() * splashes.length)];
+    let splash = splashes[Math.floor(Math.random() * splashes.length)];
+    document.getElementById("splash").innerHTML = replaceSplash(splash);
 }
 randomSplash();
 document.getElementById("randomSplash").addEventListener("click", randomSplash);
